9a8f1e6bc55ada1489c9d103b9e98882aa28a720
nhee0410 <nhee0410@pusan.ac.kr>
2022-07-25 22:03:52 +0900

    [S07P12D108-133] feat: 태그 수정 | menu 클릭 이벤트 구현

--- Sduty_User/app/src/main/java/com/d108/sduty/ui/main/home/StoryRegisterFragment.kt
+++ Sduty_User/app/src/main/java/com/d108/sduty/ui/main/home/StoryRegisterFragment.kt
@@ -1,19 +1,21 @@
 package com.d108.sduty.ui.main.home
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.MenuInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.widget.PopupMenu
 import com.d108.sduty.R
 import com.d108.sduty.databinding.FragmentStoryRegisterBinding
+import com.d108.sduty.utils.showToast
 
 //게시물 등록 - 글 내용입력, 이미지 추가/ 미리보기, 카메라 or 이미지 선택, 태그 선택
 private const val TAG ="StoryRegisterFragment"
-class StoryRegisterFragment : Fragment() {
+class StoryRegisterFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
     private lateinit var binding: FragmentStoryRegisterBinding
+    // (공개 범위) 0 : 전체 공개, 1 : 팔로워만, 2 : 나만 보기
+    private var disclosure = 0
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,17 +24,42 @@ class StoryRegisterFragment : Fragment() {
         binding = FragmentStoryRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            // 공개 범위 설정 버튼 클릭 시, 팝업 메뉴 보이기
+            btnDisclosure.setOnClickListener {
+                PopupMenu(requireContext(), it).apply {
+                    // implements OnMenuItemClickListener
+                    setOnMenuItemClickListener(this@StoryRegisterFragment)
+                    inflate(R.menu.disclosure_menu)
+                    show()
+                }
+            }
         }
     }
 
-    fun showDisclosurePopup(v: View) {
-        val popup = PopupMenu(requireContext(), v)
-        val inflater: MenuInflater = popup.menuInflater
-        inflater.inflate(R.menu.disclosure_menu, popup.menu)
-        popup.show()
+    // 팝업 메뉴 클릭 이벤트 설정
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.publicDisclosure -> {
+                disclosure = 0
+                requireContext().showToast("전체 공개 클릭 : " + disclosure)
+                true
+            }
+            R.id.followerDisclosure -> {
+                disclosure = 1
+                requireContext().showToast("팔로워만 공개 클릭 : " + disclosure)
+                true
+            }
+            R.id.privateDisclosure -> {
+                disclosure = 2
+                requireContext().showToast("나만 보기 클릭 : " + disclosure)
+                true
+            }
+            else -> false
+        }
     }
 }
