1d444bada437e56ff8fb3c6f51ea6a4ad5e1ed2c
JeongBJ <ebjdev@gmail.com>
2022-07-25 17:44:31 +0900

    [S07P12D108-84] feat : 사용자 프로필 | 생성

--- Sduty_User/app/src/main/java/com/d108/sduty/ui/sign/ProfileRegistFragment.kt
+++ Sduty_User/app/src/main/java/com/d108/sduty/ui/sign/ProfileRegistFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.d108.sduty.R
 import com.d108.sduty.databinding.FragmentProfileRegistBinding
+import com.d108.sduty.ui.viewmodel.MainViewModel
 
 //프로필 등록 - 프로필 사진, 별명, 직업, 관심 분야, 생년월일, 자기소개
 private const val TAG = "ProfileRegistFragment"
 class ProfileRegistFragment : Fragment() {
     private lateinit var binding: FragmentProfileRegistBinding
+    private val mainViewModel: MainViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
