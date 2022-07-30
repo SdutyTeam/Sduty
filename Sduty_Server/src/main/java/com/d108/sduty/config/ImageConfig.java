9d9262eba2c28c88fb379e3eabbe0d4423fad311
정봉진 <ebjdev@gmail.com>
2022-07-31 02:01:12 +0900

    [S07P12D108-228] feat : 이미지 매핑

---
+++ Sduty_Server/src/main/java/com/d108/sduty/config/ImageConfig.java
@@ -0,0 +1,17 @@
+package com.d108.sduty.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ImageConfig implements WebMvcConfigurer{
+    private String connectPath = "/images/**";
+    private String resourcePath = "file:////home/ubuntu/S07P12D108/Sduty_Server/src/main/resources/image/";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(connectPath)
+                .addResourceLocations(resourcePath);
+    }
+}
