87e6e3cba3b8092f6c23f6d543907394601433ba
정봉진 <ebjdev@gmail.com>
2022-07-30 13:42:11 +0900

    [S07P12D108-117] fix: 스터디 등록 | 서버 수정

--- Sduty_Server/src/main/java/com/d108/sduty/dto/User.java
+++ Sduty_Server/src/main/java/com/d108/sduty/dto/User.java
@@ -60,7 +60,7 @@ public class User {
 				+ email + ", fcmToken=" + fcmToken + ", regtime=" + regtime + ", userPublic=" + userPublic + "]";
 	}
 	
-	@OneToMany(mappedBy="masterSeq", fetch = FetchType.LAZY)
+	@OneToMany(mappedBy="masterSeq", fetch = FetchType.EAGER)
 	private Set<Study> masterStudies  = new HashSet<Study>();
 	
 	@ManyToMany(mappedBy="participation", fetch = FetchType.EAGER)
