5028a20531209e73779eef1068302155cb6120ca
정봉진 <ebjdev@gmail.com>
2022-08-01 00:40:25 +0900

    [S07P12D108-183] feat : 업적 | UI 완료

--- Sduty_User/app/src/main/java/com/d108/sduty/ui/main/mypage/AchievementFragment.kt
+++ Sduty_User/app/src/main/java/com/d108/sduty/ui/main/mypage/AchievementFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.d108.sduty.R
+import com.d108.sduty.adapter.AchievementAdapter
 import com.d108.sduty.databinding.FragmentAchievementBinding
+import com.d108.sduty.model.dto.Achievement
+import com.d108.sduty.ui.main.mypage.dialog.DialogAchivement
+import com.d108.sduty.utils.safeNavigate
 
 // 업적 페이지 - 업적 내용, 달성한 업적, 달성 가능한 업적, 숨겨진 업적 표시
 private const val TAG ="AchievementFragment"
 class AchievementFragment : Fragment() {
     private lateinit var binding: FragmentAchievementBinding
+    private lateinit var achievementAdapter: AchievementAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,5 +30,36 @@ class AchievementFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initViewModel()
+
     }
+
+    private fun initView() {
+        achievementAdapter = AchievementAdapter(requireActivity())
+        achievementAdapter.onClickListener = object : AchievementAdapter.OnClickListener{
+            override fun onClick(view: View, position: Int) {
+                DialogAchivement(requireContext()).let {
+                    it.show(parentFragmentManager.beginTransaction(), null)
+                }
+            }
+        }
+        binding.apply {
+            lifecycleOwner = this@AchievementFragment
+            recyclerAchievement.apply {
+                layoutManager = GridLayoutManager(requireContext(), 3)
+                adapter = achievementAdapter
+            }
+        }
+    }
+
+    private fun initViewModel() {
+        val list = mutableListOf<Achievement>()
+        for(i in 0.. 25){
+            list.add(Achievement(i, "업적 ${i}","업적", 0))
+        }
+        achievementAdapter.list = list
+    }
+
 }
