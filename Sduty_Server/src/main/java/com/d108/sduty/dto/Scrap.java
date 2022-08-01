8f448ef7e5a0bb3b3d14c634d2a61553b1c1a26b
최영희 <chanatonya@naver.com>
2022-08-01 17:40:40 +0900

    [S07P12D108-240] feat: 타임라인 작성 / 특정 유저 타임라인 조회

---
+++ Sduty_Server/src/main/java/com/d108/sduty/dto/Scrap.java
@@ -0,0 +1,25 @@
+package com.d108.sduty.dto;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Scrap {
+	@Id
+	private int userSeq;
+	private int storySeq;
+}
