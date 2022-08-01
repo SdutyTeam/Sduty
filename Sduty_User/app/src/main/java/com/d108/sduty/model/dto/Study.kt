17a336f7881e9b933d111662b0c1e896134035ef
JeongBJ <ebjdev@gmail.com>
2022-08-01 12:25:38 +0900

    [S07P12D108-217] feat : 설정 | 앱 잠금 추가

--- Sduty_User/app/src/main/java/com/d108/sduty/model/dto/Study.kt
+++ Sduty_User/app/src/main/java/com/d108/sduty/model/dto/Study.kt
@@ -14,13 +14,13 @@ data class Study(
     var password: String,
     var studyRegtime: Date?,
     var notice: String,
-    var camstudy: Boolean
+    var roomId: String?
 ) {
     constructor(
         masterSeq: Int, name: String, introduce: String, category: String, limitNumber: Int,
         password: String, camstudy: Boolean):
             this(0, masterSeq, name, introduce, category,
-            limitNumber, 1, password, null, "", camstudy)
+            limitNumber, 1, password, null, "", null)
 
     constructor(): this(0,"구미 1반 8팀","안녕하세요","SSAFY",6,"권용준",true) // 영상촬영용. 삭제해야됨
 
