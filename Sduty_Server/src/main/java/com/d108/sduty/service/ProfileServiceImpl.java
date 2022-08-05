23e0f72194f4e426ea54415730209aeb9ffbbfea
최영희 <chanatonya@naver.com>
2022-08-05 19:46:56 +0900

    [S07P12D108-228] feat: 유저 관심 분야 태그 저장, 조회, 수정

--- Sduty_Server/src/main/java/com/d108/sduty/service/ProfileServiceImpl.java
+++ Sduty_Server/src/main/java/com/d108/sduty/service/ProfileServiceImpl.java
@@ -1,21 +1,36 @@
 package com.d108.sduty.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.d108.sduty.dto.Profile;
+import com.d108.sduty.dto.UserInterest;
 import com.d108.sduty.repo.ProfileRepo;
+import com.d108.sduty.repo.UserInterestRepo;
 
 @Service
 public class ProfileServiceImpl implements ProfileService {
 	@Autowired
-	ProfileRepo profileRepo;
+	private ProfileRepo profileRepo;
 
+	@Autowired
+	private UserInterestRepo userInterestRepo;
+	
 	@Override
 	public Profile insertProfile(Profile profile) throws Exception {
-		return profileRepo.save(profile);
+		Profile p = profileRepo.save(profile);
+		if(p != null) {
+			if(!profile.getInterestHashtag().isEmpty()) {
+				for(UserInterest ui : profile.getInterestHashtag()) {
+					userInterestRepo.save(ui);
+				}
+			}
+		}
+		return p;
 	}
 
 	@Override
@@ -24,12 +39,32 @@ public class ProfileServiceImpl implements ProfileService {
 	}
 
 	@Override
-	public Optional<Profile> selectProfile(int seq) {
-		return profileRepo.findById(seq);
+	public Profile selectProfile(int userSeq) {
+		List<UserInterest> listUI = userInterestRepo.findAllByUserSeq(userSeq);
+		Optional<Profile> OProfile =  profileRepo.findById(userSeq);
+		Profile p;
+		if(OProfile.isPresent()) {
+			p = OProfile.get();
+			p.setInterestHashtag(listUI);
+			return p;
+		}
+		return null;
 	}
 
 	@Override
 	public Profile updateProfile(Profile profile) throws Exception {
-		return profileRepo.save(profile);
+		int userSeq = profile.getUserSeq();
+		for(UserInterest ui : userInterestRepo.findAllByUserSeq(userSeq)) {
+			userInterestRepo.delete(ui);
+		}
+		Profile p = profileRepo.save(profile);
+		if(p != null) {
+			if(!profile.getInterestHashtag().isEmpty()) {
+				for(UserInterest ui : profile.getInterestHashtag()) {
+					userInterestRepo.save(ui);
+				}
+			}
+		}
+		return p;
 	}
 }
