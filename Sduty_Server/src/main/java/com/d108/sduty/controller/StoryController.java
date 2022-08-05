def397629257fca9092b2a53747be06a4157a318
최영희 <chanatonya@naver.com>
2022-08-05 18:06:21 +0900

    [S07P12D108-252] fix: 저장 코드 수정3

--- Sduty_Server/src/main/java/com/d108/sduty/controller/StoryController.java
+++ Sduty_Server/src/main/java/com/d108/sduty/controller/StoryController.java
@@ -141,8 +141,9 @@ public class StoryController {
 		//imageService.fileUpload(mpfImage); - makeThumbnail()에서 저장.
 		
 		Story result = storyService.insertStory(story);
-		if(result != null) {
-			return new ResponseEntity<Story>(story, HttpStatus.OK);
+		Timeline t = timelineService.selectDetailTimeline(result.getSeq());
+		if(t != null) {
+			return new ResponseEntity<Timeline>(t, HttpStatus.OK);
 		}
 		return new ResponseEntity<Void>(HttpStatus.UNAUTHORIZED);
 	}
@@ -163,7 +164,14 @@ public class StoryController {
 		if(savedStory != null) {
 			story.setImageSource(savedStory.getImageSource());
 			story.setThumbnail(savedStory.getThumbnail());
-			return new ResponseEntity<Story>(storyService.updateStory(story), HttpStatus.OK);
+			
+			Story result = storyService.updateStory(story);
+			if(result != null) {
+				Timeline t = timelineService.selectDetailTimeline(result.getSeq());
+				if(t != null) {
+					return new ResponseEntity<Timeline>(t, HttpStatus.OK);
+				}
+			}
 		}
 		return new ResponseEntity<Void>(HttpStatus.UNAUTHORIZED);
 	}
