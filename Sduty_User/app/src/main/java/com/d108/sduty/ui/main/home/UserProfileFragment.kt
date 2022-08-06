90ee8b080cd8e5917d3f6179a631e4ffbe1aca49
정봉진 <ebjdev@gmail.com>
2022-08-07 04:55:28 +0900

    [S07P12D108-143] fix : 팔로우 | UserProfile 메뉴 팔로우

--- Sduty_User/app/src/main/java/com/d108/sduty/ui/main/home/UserProfileFragment.kt
+++ Sduty_User/app/src/main/java/com/d108/sduty/ui/main/home/UserProfileFragment.kt
@@ -1,29 +1,38 @@
 package com.d108.sduty.ui.main.home
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.*
+import androidx.annotation.IdRes
+import androidx.appcompat.widget.PopupMenu
+import androidx.core.os.bundleOf
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
+import com.d108.sduty.R
 import com.d108.sduty.adapter.StoryAdapter
+import com.d108.sduty.common.NOT_PROFILE
 import com.d108.sduty.databinding.FragmentUserProfileBinding
 import com.d108.sduty.model.dto.Follow
+import com.d108.sduty.model.dto.InterestHashtag
+import com.d108.sduty.model.dto.JobHashtag
 import com.d108.sduty.model.dto.Story
+import com.d108.sduty.ui.sign.dialog.TagSelectDialog
 import com.d108.sduty.ui.viewmodel.MainViewModel
 import com.d108.sduty.ui.viewmodel.StoryViewModel
 import com.d108.sduty.utils.safeNavigate
+import com.d108.sduty.utils.showAlertDialog
+import com.d108.sduty.utils.showToast
 
 //사용자 프로필 - 사용자 닉네임, 프로필 사진, 숫자 표시(게시물, 팔로우, 팔로워), 한줄소개,
 // 잔디그래프,게시물(그리드+스크롤) , 더 보기, 업적
 private const val TAG = "UserProfileFragment"
-class UserProfileFragment : Fragment() {
+class UserProfileFragment : Fragment(), PopupMenu.OnMenuItemClickListener   {
     private lateinit var binding: FragmentUserProfileBinding
     private val viewModel: StoryViewModel by viewModels()
     private val mainViewModel: MainViewModel by activityViewModels()
@@ -81,6 +90,34 @@ class UserProfileFragment : Fragment() {
                 viewModel.doFollow(Follow(mainViewModel.user.value!!.seq, viewModel.profile.value!!.userSeq))
                 mainViewModel.getProfileValue(mainViewModel.user.value!!.seq)
             }
+
+            ivSetting.setOnClickListener {
+                PopupMenu(requireContext(), it).apply {
+                    setOnMenuItemClickListener(this@UserProfileFragment)
+                    inflate(R.menu.menu_story_visiters)
+                    if (mainViewModel.checkFollowUser(viewModel.profile.value!!.userSeq)) {
+                        menu[0].title = "언팔로우"
+                    }else{
+                        menu[0].title = "팔로우"
+                    }
+                    show()
+                }
+            }
         }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item!!.itemId) {
+            R.id.follow -> {
+                viewModel.doFollow(Follow(mainViewModel.user.value!!.seq, viewModel.profile.value!!.userSeq))
+                mainViewModel.getProfileValue(mainViewModel.user.value!!.seq)
+                if (mainViewModel.checkFollowUser(viewModel.profile.value!!.userSeq)) {
+                    item.title = "언팔로우"
+                }else{
+                    item.title = "팔로우"
+                }
+            }
+        }
+        return true
     }
 }
