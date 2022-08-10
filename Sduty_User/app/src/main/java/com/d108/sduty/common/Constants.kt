088a631e490ab17b735489f7ca385bf8d2cca003
JeongBJ <ebjdev@gmail.com>
2022-08-10 12:44:18 +0900

    [S07P12D108-20] fix : 스터디 | 캠스터디 수정

--- Sduty_User/app/src/main/java/com/d108/sduty/common/Constants.kt
+++ Sduty_User/app/src/main/java/com/d108/sduty/common/Constants.kt
@@ -7,7 +7,7 @@ const val SOLAPI_API_KEY = "NCSOR6GTURVMA4XT"
 const val SOLAPI_API_SECRET_KEY = "BENC5C7DNBGXLVJLZLLTN5WB5FSJCDOL"
 const val SOLAPI_DOMAIN = "https://api.solapi.com"
 
-const val SENDBIRD_APP_ID = "A5324725-2AC3-4971-AF99-D0CB863D0717"
+const val SENDBIRD_APP_ID = "2968122E-E32C-4E69-A16A-A6C173AAA1C2"
 const val UNKNOWN_SENDBIRD_ERROR = "unknown sendbird error"
 const val EXTRA_ROOM_ID = "com.sendbird.calls.quickstart.ROOM_ID"
 const val EXTRA_IS_NEWLY_CREATED = "com.sendbird.calls.quickstart.IS_NEWLY_CREATED"
