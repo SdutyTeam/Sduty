27c56dcfb5abfcd8c49ce025e43920bfe39e5639
정봉진 <ebjdev@gmail.com>
2022-08-15 11:29:00 +0900

    no message

--- Sduty_Server/src/main/java/com/d108/sduty/controller/WebController.java
+++ Sduty_Server/src/main/java/com/d108/sduty/controller/WebController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebController {
 	@RequestMapping("/privacy")
 	public String index() {
+		System.out.println("asldkjflkasdjflkajsdf");
 		return "index";
 	}
 }
