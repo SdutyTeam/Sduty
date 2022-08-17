b66e31a17df1a25faa34d5f9f2d647b62fbba92f
편예린 <dpfls3433@gmail.com>
2022-08-17 10:11:16 +0900

    [S07P12D108-312] fix: properties파일 암호화 | 완료

--- Sduty_Server/src/main/java/com/d108/sduty/config/JasyptConfig.java
+++ Sduty_Server/src/main/java/com/d108/sduty/config/JasyptConfig.java
@@ -3,12 +3,15 @@ package com.d108.sduty.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
-	private final static String KEY = "sdutyd108";//이건 어떻게하지
+	@Value("${jasypt.encryptor.password}")
+	private String KEY;
+	//private final static String KEY = "sdutyd108";
 	
 	@Bean("jasyptStringEncryptor")
 	public StringEncryptor stringEncryptor() {
