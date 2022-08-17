219a01f2c2dbb9d396aad4589a8f389bd587a25a
DESKTOP-KVCQHCD\multicampus <wkdrns3918@naver.com>
2022-08-17 15:31:37 +0900

    [AOS] 관리자 : 공지사항 등록/수정/삭제

--- Sduty_Admin/app/src/main/java/com/d108/sduty_admin/ui/MainViewModel.kt
+++ Sduty_Admin/app/src/main/java/com/d108/sduty_admin/ui/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.d108.sduty_admin.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.d108.sduty_admin.model.dto.Admin
 
 class MainViewModel: ViewModel() {
 
@@ -13,4 +14,9 @@ class MainViewModel: ViewModel() {
     fun setVisibilityBottomNav(show: Boolean){
         _visibilityBottomNav.postValue(show)
     }
+
+    private val _admin = MutableLiveData<Admin>()
+    val admin: LiveData<Admin>
+        get() = _admin
+    fun setAdminValue(admin: Admin){ _admin.postValue(admin)}
 }
