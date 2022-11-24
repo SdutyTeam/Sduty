deb8f780026c34f853a9304bf84fa4aadb08fff0
JeongBJ <ebjdev@gmail.com>
2022-11-24 17:05:29 +0900

    UPDATE : Server | Image 경로 수정

--- Sduty_Server_v2/Sduty_Server/src/main/java/com/d108/sduty/config/ImageConfig.java
+++ Sduty_Server_v2/Sduty_Server/src/main/java/com/d108/sduty/config/ImageConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ImageConfig implements WebMvcConfigurer{
     private String connectPath = "/image/**";
     //private String resourcePath = "file:////home/ubuntu/docker-volume/jenkins/workspace/k205sduty/SDUTY/Sduty_Server/src/main/resources/image/";
-    private String resourcePath = "file:////home/files/profile";
+    private String resourcePath = "file:////home/files/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
